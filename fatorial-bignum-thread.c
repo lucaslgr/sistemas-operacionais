6732f4bc985de652e2201c7fc11d9da376e54a55
lucaslgr <lucaslgr1206@gmail.com>
2020-09-17 19:47:41 -0300

    Exercicio finalizado para 3 threads com mensuracao do tempo gasto para o calculo de fatorial sequencial e com 3 threads

--- fatorial-bignum-thread.c
+++ fatorial-bignum-thread.c
@@ -7,11 +7,13 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include<pthread.h> //para criar threads
-#include "./lib/bignum.c"
+#include <pthread.h>      //para criar threads
+#include "./lib/bignum.c" //lib não nativa para utilizar o tipo bignum
+#include <time.h>         //Utilizada para tempo
 
 //* Estrutura que armazena os fornece e armazena os dados de cada thread
-struct data_task {
+struct data_task
+{
     int *p_offsets;
     int indicator_part;
     bignum partial_result;
@@ -19,8 +21,9 @@ struct data_task {
 };
 
 //*Tarefa executada por cada thread
-void* task_thread(void *task_dt){
-    struct data_task * p_data_task = (struct data_task *) task_dt;
+void *task_thread(void *task_dt)
+{
+    struct data_task *p_data_task = (struct data_task *)task_dt;
 
     int_to_bignum(1, &p_data_task->partial_result);
 
@@ -28,18 +31,17 @@ void* task_thread(void *task_dt){
     int upper_limit = p_data_task->p_offsets[p_data_task->indicator_part];
 
     int inferior_limit = 1;
-    if(p_data_task->indicator_part > 0){
-        inferior_limit = p_data_task->p_offsets[p_data_task->indicator_part -1];
+    if (p_data_task->indicator_part > 0)
+    {
+        inferior_limit = p_data_task->p_offsets[p_data_task->indicator_part - 1];
     }
 
     printf(
         "\r\n====================================Thread %d INFO====================================\n\r",
-        p_data_task->indicator_part+1
-    );
+        p_data_task->indicator_part + 1);
     printf(
         "Indicator %d | Upper_Limit %d | Inferior_Limit %d \n\r",
-        p_data_task->indicator_part, upper_limit, inferior_limit
-    );
+        p_data_task->indicator_part, upper_limit, inferior_limit);
 
     for (int i = upper_limit; i > inferior_limit; i--)
     {
@@ -54,25 +56,28 @@ void* task_thread(void *task_dt){
 
     printf(
         "Resultado parcial do fatorial pela thread %d : ",
-        p_data_task->indicator_part
-    );
+        p_data_task->indicator_part);
     print_bignum(&p_data_task->partial_result);
     printf("\n\n\r\r");
 }
 
 //Identifica o fatorial e separa em 3 para ser calculado por cada thread
-void split_fatorial(int num, int *fac_offset){
+void split_fatorial(int num, int *fac_offset)
+{
     int rest;
-    rest = num%3;
+    rest = num % 3;
 
-    if(rest == 0){
-        int offset = num/3;
+    if (rest == 0)
+    {
+        int offset = num / 3;
 
         fac_offset[0] = offset;
-        fac_offset[1] = offset*2;
-        fac_offset[2] = offset*3;
-    } else {
-        int offset = (int) num/3;
+        fac_offset[1] = offset * 2;
+        fac_offset[2] = offset * 3;
+    }
+    else
+    {
+        int offset = (int)num / 3;
 
         fac_offset[0] = offset + rest; //Adicionando o resto para completar o número quebrado
         fac_offset[1] = fac_offset[0] + offset;
@@ -80,52 +85,65 @@ void split_fatorial(int num, int *fac_offset){
     }
 }
 
-
 //Verifica se o número é 0, 1 ou 2. Se for, já retorna o resultado
-void solve_if_small_number(int *num){
-    
-    if(*num == 0 || *num == 1){
+void solve_if_small_number(int *num)
+{
+
+    if (*num == 0 || *num == 1)
+    {
         printf("Resposta: %d", 1);
         exit;
         return;
-    } else if(*num == 2){
+    }
+    else if (*num == 2)
+    {
         printf("Resposta: %d", 2);
         exit;
         return;
     }
 }
 
-
 //Multiplica todos os resultados parciais do fatorial
-void join_partial_results(bignum *a, bignum *b, bignum *c, bignum *final_result){
+void join_partial_results(bignum *a, bignum *b, bignum *c, bignum *final_result)
+{
     bignum partial_result;
     multiply_bignum(a, b, &partial_result);
     multiply_bignum(&partial_result, c, final_result);
 }
 
-//!OBS: Primeiro argumento do argv é o numero a se calcular o fatorial
-int main(int argc, char *argv[]){
+//!OBS:
+//!Primeiro argumento do argv é o numero a se calcular o fatorial
+//!Segundo argumento do argv é o numero de threads que serão utilizadas para o calculo
+int main(int argc, char *argv[])
+{
     //===================================<CÁLCULO DO FATORIAL COM BIGNUM>===================================
     //pritnf("Informe o numero que deseja calcular o fatorial: ");
     //scanf("%d", &num);
 
+    //Variaveis para armazenar os clocks do processador para medir o tempo no final
+    clock_t start, end;
+
     bignum bignum_n;
     int num;
 
-    //Convertendo de char para inteiro
+    //Convertendo de char para inteiro, agrv[1] é referente ao número para se calcular o fatorial
     num = atoi(argv[1]);
-    
     printf("Calculando fatorial de: %d \n\r", num);
-    
+
+    //Inicializando a contagem de clocks
+    start = clock();
+
+    //Resolvando o fatorial caso o num desejado seja 0,1 ou 2
     solve_if_small_number(&num);
 
+    //Vetor que armazena o cada número que limitara até onde cada thread deve calcular
     int fac_offset[3];
+
     //Divindo o número em partes iguais para enviar para cada thread
     split_fatorial(num, fac_offset);
 
     //Setando os dados de cada estrutura que cada thread vai resolver
     struct data_task dt1, dt2, dt3;
-
     dt1.indicator_part = 0;
     dt1.p_offsets = fac_offset;
 
@@ -139,17 +157,26 @@ int main(int argc, char *argv[]){
     pthread_t t1, t2, t3;
 
     // cria as tarefas
-    pthread_create(&t1, NULL, task_thread, (void *)&dt1); 
-    pthread_create(&t2, NULL, task_thread, (void *)&dt2); 
-    pthread_create(&t3, NULL, task_thread, (void *)&dt3); 
+    pthread_create(&t1, NULL, task_thread, (void *)&dt1);
+    pthread_create(&t2, NULL, task_thread, (void *)&dt2);
+    pthread_create(&t3, NULL, task_thread, (void *)&dt3);
     pthread_join(t1, NULL);
     pthread_join(t2, NULL);
     pthread_join(t3, NULL);
 
     bignum final_result;
     join_partial_results(&dt1.partial_result, &dt2.partial_result, &dt3.partial_result, &final_result);
-    
-    printf("Resultado Final: \r\n");
+
+    //Finalizando a contagem de clocks
+    end = clock();
+
+    //Calculando o tempo decorrido
+    double tmp = (double)(end - start);
+    tmp = tmp / (double)CLOCKS_PER_SEC;
+    printf("\nTempo decorrido: %f\n", tmp);
+
+    //Printando o resultado final
+    printf("Resultado Final: ");
     print_bignum(&final_result);
 
     return 0;
--- fatorial-bignum.c
+++ fatorial-bignum.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
-#include "./lib/bignum.c"
+#include "./lib/bignum.c" //lib não nativa para utilizar o tipo bignum
+#include <time.h>         //Utilizada para tempo
 
 //*Calcula fatorial recursivamente
-bignum calc_fatorial(bignum bignum_n){
-    bignum bignum_zero, bignum_one,bignum_two;
+bignum calc_fatorial(bignum bignum_n)
+{
+    bignum bignum_zero, bignum_one, bignum_two;
     int_to_bignum(0, &bignum_zero);
     int_to_bignum(1, &bignum_one);
     int_to_bignum(2, &bignum_two);
@@ -13,14 +15,17 @@ bignum calc_fatorial(bignum bignum_n){
     bignum bignum_n_copy;
     bignum_n_copy = bignum_n;
 
-    if(compare_bignum(&bignum_zero, &bignum_n) == 0 || compare_bignum(&bignum_one, &bignum_n) == 0){
+    if (compare_bignum(&bignum_zero, &bignum_n) == 0 || compare_bignum(&bignum_one, &bignum_n) == 0)
+    {
         return bignum_one;
-    } else{
+    }
+    else
+    {
         bignum bignum_n_decremented;
         subtract_bignum(&bignum_n_copy, &bignum_one, &bignum_n_decremented);
 
         bignum partial_result = calc_fatorial(bignum_n_decremented);
-        
+
         bignum bignum_result;
 
         multiply_bignum(&partial_result, &bignum_n, &bignum_result);
@@ -30,7 +35,8 @@ bignum calc_fatorial(bignum bignum_n){
 }
 
 //!OBS: Primeiro argumento do argv é o numero a se calcular o fatorial
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[])
+{
     //===================================<CÁLCULO DO FATORIAL COM BIGNUM>===================================
     //pritnf("Informe o numero que deseja calcular o fatorial: ");
     //scanf("%d", &num);
@@ -40,8 +46,12 @@ int main(int argc, char *argv[]){
 
     //Convertendo de char para inteiro
     num = atoi(argv[1]);
+
+    //Variaveis para armazenar os clocks do processador para medir o tempo no final
+    clock_t start, end;
+
     //Convertendo de inteiro para bignum
-    int_to_bignum(num,&bignum_n);
+    int_to_bignum(num, &bignum_n);
 
     printf("Valor informado foi: %d \n\r", num);
 
@@ -49,10 +59,19 @@ int main(int argc, char *argv[]){
     printf("O limite máximo do valor de um inteiro nessa máquina é: %d\r\n", INT_MAX);
 
     //printf("Resultado: %d\n\r", fat);
-    
+
     printf("Resultado: \r\n");
 
     bignum result = calc_fatorial(bignum_n);
+
+    //Finalizando a contagem de clocks
+    end = clock();
+
+    //Calculando o tempo decorrido
+    double tmp = (double)(end - start);
+    tmp = tmp / (double)CLOCKS_PER_SEC;
+    printf("\nTempo decorrido: %f\n", tmp);
+
     print_bignum(&result);
 
     return 0;
